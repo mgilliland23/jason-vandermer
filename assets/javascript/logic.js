717eb9ac8953dd1d508c9fc0fe64852e10db5023
Matt Gilliland <mgilliland23@gmail.com>
2019-06-15 18:16:52 -0400

    changed logic from jQuery to vanilla JS

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,28 +1,30 @@
-$("img").on("click", function () {
-    //inject url to vimeo video (add attribute to each image):
-    //$(this).attr('video-url')
-    console.log("thumbnail clicked");
-});
+var img = document.getElementsByTagName("img");
 
-$("img").hover(enterHover, exitHover);
+console.log(img);
+
+
+for (var i = 0; i < img.length; i++) {
+    img[i].addEventListener("mouseover", enterHover);
+    img[i].addEventListener("mouseout", exitHover);
+};
 
 function enterHover() {
     console.log("enter")
-    var currentVideoContainer = $(this).parent();
-    var director = currentVideoContainer.find(".director");
-    var info = currentVideoContainer.find(".info");
+    var currentVideoContainer = event.target.parentElement;
 
-    director.css('color', 'white');
-    info.css('color', 'white');
+    var director = currentVideoContainer.getElementsByClassName("director")[0];
+    var info = currentVideoContainer.getElementsByClassName("info")[0];
 
+    director.style.color = "white";
+    info.style.color = "white";
 }
 
 function exitHover() {
-    var currentVideoContainer = $(this).parent();
-    var director = currentVideoContainer.find(".director");
-    var info = currentVideoContainer.find(".info");
+    var currentVideoContainer = event.target.parentElement;
 
-    info.css('color', 'black');
-    director.css('color', 'black');
+    var director = currentVideoContainer.getElementsByClassName("director")[0];
+    var info = currentVideoContainer.getElementsByClassName("info")[0];
 
+    director.style.color = "black";
+    info.style.color = "black";
 }
