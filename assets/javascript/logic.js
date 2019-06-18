e92234640c525bf971f474f804c1d5a8ae0f7529
Matt Gilliland <mgilliland23@gmail.com>
2019-06-18 12:13:08 -0400

    fixed bug: video still playing in background after modal closed

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -21,22 +21,34 @@ function enterHover() {
 function exitHover() {
     var currentVideoContainer = event.target.parentElement;
 
-    var director = currentVideoContainer.getElementsByClassName("director")[0];
-    var info = currentVideoContainer.getElementsByClassName("info")[0];
+    var director = currentVideoContainer.getElementsByClassName('director')[0];
+    var info = currentVideoContainer.getElementsByClassName('info')[0];
 
-    director.style.color = "black";
-    info.style.color = "black";
+    director.style.color = 'black';
+    info.style.color = 'black';
 }
 
 function displayVideo() {
     var videoPreviewImg = event.target;
-    var videoURL = videoPreviewImg.getAttribute("data-videoURL");
-
+    var videoURL = videoPreviewImg.getAttribute('data-videoURL');
 
     var modalVideo = document.getElementById('video-player');
-
     modalVideo.src = videoURL;
 
     $("#vimeo-modal").modal();
 
+
+}
+
+$('#vimeo-modal').on('hidden.bs.modal', function (e) {
+    stopVideoOnExit();
+})
+
+function stopVideoOnExit() {
+    var modal = document.getElementById('vimeo-modal');
+
+    var video = modal.querySelector('iframe');
+    console.log(video);
+    //Just remove the iFrame from the DOM to get it to stop playing
+    video.parentNode.removeChild(video);
 }
